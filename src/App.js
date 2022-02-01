3cc7eaf026e9108b81db99fe48df8fe7e9dcf9f0
DavidH1982 <david.houghton1982@gmail.com>
2022-02-01 20:27:51 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [song, setSong] = useState({});
+  const [mood, setMood] = useState("");
+
+  const limit = 10;
+  const random = Math.floor(Math.random() * limit + 1);
+  const baseURL = `https://api.jamendo.com/v3.0/tracks/?client_id=${process.env.REACT_APP_CLIENT_ID}&format=jsonpretty&limit=${limit}&fuzzytags=${mood}&speed=high%2Bveryhigh&include=musicinfo&groupby=artist_id`;
+
+  const handleMood = (e) => setMood(e.target.value);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(baseURL);
+      const songList = await response.json();
+      setSong(songList.results[random]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <ul className="circleLogo">
+        <li className="segment red" />
+        <li className="segment redViolet" />
+        <li className="segment violet" />
+        <li className="segment blueViolet" />
+        <li className="segment blue" />
+        <li className="segment blueGreen" />
+        <li className="segment green" />
+        <li className="segment yellowGreen" />
+        <li className="segment yellow" />
+        <li className="segment yellowOrange" />
+        <li className="segment orange" />
+        <li className="segment redOrange" />
+      </ul>
     </div>
+    <div className="App">
+      <h1>🎵 Moodplayer 🎶</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="mood">What mood are you?</label>
+        <input type="text" name="mood" value={mood} onChange={handleMood} />
+        <input type="submit" value="Submit" />
+      </form>
+      <br />
+      <audio src={song.audio} autoPlay controls></audio>
+    </div>
+    </>
   );
 }
 
